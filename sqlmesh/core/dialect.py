4352e63b0c4c4357fb140a27623fe9e92459feba
Denis Gribanov <itsme@petrikoro.com>
2026-02-27 03:15:47 +0300

    chore(audits): preserve comments above AUDIT during formatting

--- sqlmesh/core/dialect.py
+++ sqlmesh/core/dialect.py
@@ -1149,6 +1149,7 @@ def extend_sqlglot() -> None:
                 *generator.WITH_SEPARATED_COMMENTS,
                 Model,
                 MacroDef,
+                Audit,
             )
 
         generator.UNWRAPPED_INTERVAL_VALUES = (
